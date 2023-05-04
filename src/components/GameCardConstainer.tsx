1326f127030e6071ec6eb12e3a549c360686cd05
Manoochehr Khatami <manouchehr.khatami@gmail.com>
2023-05-04 10:02:12 +0200

    Disply genres

--- src/components/GameCardConstainer.tsx
+++ src/components/GameCardConstainer.tsx
@@ -7,7 +7,6 @@ interface Props {
 const GameCardConstainer = ({ children }: Props) => {
   return (
     <Box
-      width="260px"
       boxShadow={"0px 0px 20px -2px #888888"}
       borderRadius={10}
       overflow={"hidden"}
